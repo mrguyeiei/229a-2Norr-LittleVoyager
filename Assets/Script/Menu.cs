d2dab96c6e86211b3256373dd463cbc376c047a1
mrguyeiei <thanachot.phiw@bumail.net>
2025-05-06 15:43:26 +0700

    90%

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -13,4 +13,8 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene("Start");
     }
+    public void Next()
+    {
+        SceneManager.LoadScene("End");
+    }
 }
--- Assets/Script/YourWin.cs
+++ Assets/Script/YourWin.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class YourWin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PlayerMove component = other.gameObject.GetComponent<PlayerMove>();
+        if (component != null)
+        {
+            SceneManager.LoadScene("YourWin");
+        }
     }
 }
